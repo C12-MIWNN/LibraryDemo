8ac64c14e26b7f019f5eddf37f86fb23fd5dafd0
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-10-16 14:28:56 +0200

    Initial commit

---
+++ src/main/java/nl/miwnn/se12/vincent/LibraryDemo/LibraryDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se12.vincent.LibraryDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se12/vincent/LibraryDemo/LibraryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se12.vincent.LibraryDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
